76b35b2a8ef4847c93d688b3a1eb32628d47bba3
tomiyama <tomiyama@fe80:0:0:0:9869:95ff:feff:531d%9>
2017-05-11 15:46:35 +0900

    初回commit

---
+++ src/main/java/site/to0mi1/s3sample/S3SampleApplication.java
@@ -0,0 +1,12 @@
+package site.to0mi1.s3sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3SampleApplication.class, args);
+	}
+}
---
+++ src/test/java/site/to0mi1/s3sample/S3SampleApplicationTests.java
@@ -0,0 +1,16 @@
+package site.to0mi1.s3sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S3SampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
